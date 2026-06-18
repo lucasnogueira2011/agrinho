009a132c51464e182bd12ea3038c91700a50c959
lucas <lucas.nogueira.sousa@escola.pr.gov.br>
2026-06-18 12:28:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const brilho = document.getElementById("brilho");
+const fonte = document.getElementById("fonte");
 
+// Controle de brilho
+brilho.addEventListener("input", () => {
+  document.body.style.filter = `brightness(${brilho.value}%)`;
+});
+
+// Controle de fonte
+fonte.addEventListener("input", () => {
+  document.body.style.fontSize = fonte.value + "px";
+});
